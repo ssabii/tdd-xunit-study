6de97df0721674c567cccd58a9589e846b3eb545
ssabi <fhy1930@gmail.com>
2022-08-14 02:45:12 +0900

    19장 테이블 차리기

---
+++ ch19/xUnit.py
@@ -0,0 +1,22 @@
+class TestCase:
+  def __init__(self, name):
+    self.name = name
+  
+  def setUp(self):
+    pass
+    
+  def run(self):
+    self.setUp()
+    method = getattr(self, self.name)
+    method()
+  
+class WasRun(TestCase):
+  def __init__(self, name):
+    TestCase.__init__(self, name)
+    
+  def testMethod(self):
+    self.wasRun = 1
+  
+  def setUp(self):
+    self.wasRun = None
+    self.wasSetUp = 1
---
+++ ch19/xUnit_test.py
@@ -0,0 +1,17 @@
+from xUnit import TestCase
+from xUnit import WasRun
+
+class TestCaseTest(TestCase):
+  def setUp(self):
+    self.test = WasRun("testMethod")
+  
+  def testRunning(self):
+    self.test.run()
+    assert(self.test.wasRun)
+  
+  def testSetUp(self):
+    self.test.run()
+    assert(self.test.wasSetUp)
+    
+TestCaseTest("testRunning").run()
+TestCaseTest("testSetUp").run()
