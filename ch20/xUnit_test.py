3442068399ec24277b1b86d1a09b4190aeb62b21
ssabi <fhy1930@gmail.com>
2022-08-14 03:30:05 +0900

    20장 코드 수정

--- ch20/xUnit_test.py
+++ ch20/xUnit_test.py
@@ -2,9 +2,6 @@ from xUnit import TestCase
 from xUnit import WasRun
 
 class TestCaseTest(TestCase):
-  def setUp(self):
-    self.test = WasRun("testMethod")
-    
   def testTemplateMethod(self):
     test = WasRun("testMethod")
     test.run()
