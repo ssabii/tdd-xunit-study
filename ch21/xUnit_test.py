5439f1644717ba7dba0ba83cb85edff4959af297
ssabi <fhy1930@gmail.com>
2022-08-14 03:25:08 +0900

    21장 셈하기

---
+++ ch21/xUnit_test.py
@@ -0,0 +1,24 @@
+from xUnit import TestCase
+from xUnit import WasRun
+
+class TestCaseTest(TestCase):
+  def setUp(self):
+    self.test = WasRun("testMethod")
+    
+  def testTemplateMethod(self):
+    test = WasRun("testMethod")
+    test.run()
+    assert("setUp testMethod tearDown " == test.log)
+  
+  def testResult(self):
+    test = WasRun("testMethod")
+    result = test.run()
+    assert("1 run, 0 failed" == result.summary())
+    
+  def testFailedResult(self):
+    test = WasRun("testBrokenMethod")
+    result = test.run()
+    assert("1 run, 1 failed" == result.summary())
+    
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
